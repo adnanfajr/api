f24e0cb90b8de3a56a47e8726eb773cea32b1fce
Adnan <adnanfajri@gmail.com>
2016-09-13 07:16:31 +0700

    change

---
+++ bem_its_post.php
@@ -0,0 +1,95 @@
+<?php
+
+function scrap($page) {
+    $ch = curl_init();
+
+    if($page==='' ||$page==='1') {
+        curl_setopt($ch, CURLOPT_URL, 'http://bem.its.ac.id/id/blog-id/');
+        $halaman = 1;
+    }
+    elseif($page > 1) {
+        curl_setopt($ch, CURLOPT_URL, 'http://bem.its.ac.id/id/blog-id/page/'.$page.'/');
+        $halaman = $page;
+    }
+    else {
+        curl_setopt($ch, CURLOPT_URL, 'http://bem.its.ac.id/id/blog-id/');
+        $halaman = 1;
+    }
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // html
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    // array untuk output
+    $output = array();
+
+    // gagal cURL
+    if (!$data) {
+        $output['status'] = "error";
+        $output['pesan'] = "website sedang offline";
+    }
+    // sukses cURL
+    else {
+        // include plugin
+        require 'plugin/simple_html_dom.php';
+
+        // merubah html menjadi string
+        $html = str_get_html($data);
+
+        // menentukan bahan yang mau diolah yaitu div id=siteTable array ke 0
+        $bahan = $html->find('div[class=blog_wrapper]', 0);
+
+        // mengambil kotak yang ada di dalam siteTable yaitu div class=thing output array
+        $kotak = $bahan->find('div[class=post-item]');
+
+        // ekstrak kotak
+        foreach ($kotak as $key => $val) {
+            // Deskripsi konten
+            $desc = $val->find('div[class=post-desc-wrapper]', 0);
+
+            // Ambil judul dan link
+            $head = $desc->find('h2[class=entry-title]', 0);
+            $title = $head->find('a', 0)->innertext;
+            $link = $head->find('a', 0)->href;
+
+            // Ambil gambar
+            $img = '';
+            // Ada post yang tidak memiliki gambar
+            if (!empty($val->find('div[class=image_wrapper]', 0))) {
+                if (!empty($val->find('a', 0)->find('img', 0))) {
+                    $img = $val->find('a', 0)->find('img', 0)->src;
+                }
+            }
+
+            // Ambil author dan date
+            $auth = $val->find('div[class=author-date]', 0);
+            $author = $auth->find('a', 0)->innertext;
+            $date = $auth->find('span[class=post-date]', 0)->innertext;
+
+            $output['status'] = 'success';
+            $output['page'] = $halaman;
+            $output['data'][] = array(
+                'title' => $title,
+                'link' => $link,
+                'img' => $img,
+                'date' => $date,
+                'author' => $author
+            );
+        }
+        return $output;
+    }
+}
+
+// menentukan parameter
+$page = (empty($_GET['page']) ? '' : $_GET['page']);
+
+// menjalankan proses curl
+$result = scrap($page);
+
+// convert jadi json
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+echo json_encode($result, JSON_PRETTY_PRINT);
+?>
